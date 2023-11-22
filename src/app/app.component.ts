d9027a3100e63a1fffe406cf4ade111e01476c17
m4penta <80562619+m4penta@users.noreply.github.com>
2023-11-22 16:54:34 +0700

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { MyTableComponent } from './My-table/My-table.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
-import { MyTableComponent } from './My-table/My-table.component';
+
+
 export const routes: Routes = [
-  { path: 'my-table', component: MyTableComponent },
+
 ];
